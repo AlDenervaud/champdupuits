9a9ca9cc9f4809560fd734a3ce99154441320025
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2024-12-25 14:39:55 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -11,3 +11,14 @@ st.title(":rainbow[Bienvenue au Champs du Puits]")
 st.write("Sur ce site vous pourrez trouver la liste des produits de la ferme Au Champ du Puits, organisés par catégorie")
 st.write("Les différentes catégories sont disponibles dans le menu à gauche")
 
+
+st.markdown("## Contact")
+contact_list = """
+GAEC Au Champ du Puits
+211 chemin de la Fontaine
+01430, Peyriat
+email: ...
+Instagram: ...
+"""
+st.write(contact_list)
+

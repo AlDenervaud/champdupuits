dbb0f7a410992aad2ca79f4cc79ea6290c682cd8
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2024-12-25 14:28:51 +0100

    Create Maraichage.py

---
+++ pages/Maraichage.py
@@ -0,0 +1,10 @@
+import os
+import streamlit as st
+import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from st_aggrid.shared import JsCode, ColumnsAutoSizeMode
+
+# Title of the Streamlit app
+st.title("Liste des légumes :carrot: :tomato: :eggplant:")
+
+st.write("Bon ca va tu crois pas que c'est déjà pas mal en quelques heures ? :wink: :kissing_heart:")

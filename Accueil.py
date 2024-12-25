1a1ffa91cf7f3f9286a767678fe2212fc0bebf36
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2024-12-25 11:33:51 +0100

    Update Accueil.py
    try add image

--- Accueil.py
+++ Accueil.py
@@ -11,3 +11,6 @@ st.title("# :rainbow[Bienvenue au Champs du Puits]")
 st.write("Sur ce site vous pourrez trouver la liste des produits de la ferme Au Champ du Puits, organisés par catégorie")
 st.write("Les différentes catégories sont disponibles dans le menu à gauche")
 
+
+image_url = "https://github.com/AlDenervaud/champdupuits/main/data/images/pain_epices.jpg"
+st.image(image_url, caption="Example Image", use_column_width=True)

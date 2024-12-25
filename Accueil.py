7f532076f7479611577c36c666beacd3d4cfb840
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2024-12-25 14:47:00 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -13,7 +13,7 @@ st.write("Les différentes catégories sont disponibles dans le menu à gauche")
 
 
 st.markdown("## Contact")
-st.markdown("""<div>GAEC Au Champ du Puits<br>
+st.write("""<div>GAEC Au Champ du Puits<br>
 211 chemin de la Fontaine<br>
 01430, Peyriat</div>""")
 contact_list = """

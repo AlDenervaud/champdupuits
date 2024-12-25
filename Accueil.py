89ebb34819db888bec3fdeb185612a9f77956051
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2024-12-25 11:41:43 +0100

    Update Accueil.py
    removed images

--- Accueil.py
+++ Accueil.py
@@ -11,13 +11,3 @@ st.title("# :rainbow[Bienvenue au Champs du Puits]")
 st.write("Sur ce site vous pourrez trouver la liste des produits de la ferme Au Champ du Puits, organisés par catégorie")
 st.write("Les différentes catégories sont disponibles dans le menu à gauche")
 
-
-image_url = "https://github.com/AlDenervaud/champdupuits/blob/main/data/images/pain_epices.jpg"
-st.image(image_url, caption="test 1")
-
-image_url = "data/images/pain_epices.jpg"
-st.image(image_url, caption="test 2")
-
-from PIL import Image
-image_1=Image.open("data/images/pain_epices.jpg")
-st.image(image_1)

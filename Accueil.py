650c85d5bab04ed7fcce95185a801de73d424446
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2024-12-25 13:00:24 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -6,7 +6,7 @@ from datetime import datetime, date
 
        
 # Title of the Streamlit app
-st.title("# :rainbow[Bienvenue au Champs du Puits]")
+st.title(":rainbow[Bienvenue au Champs du Puits]")
 
 st.write("Sur ce site vous pourrez trouver la liste des produits de la ferme Au Champ du Puits, organisés par catégorie")
 st.write("Les différentes catégories sont disponibles dans le menu à gauche")

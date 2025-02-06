f662a5efc4ef08548451e8228059bbe5f785bd09
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2025-02-06 11:43:52 +0100

    Update 2_Passer commande.py

--- pages/2_Passer commande.py
+++ pages/2_Passer commande.py
@@ -147,7 +147,7 @@ try:
     order_df["Quantité"] = order_df["Quantité"].apply(lambda x: str(x))
     xx = st.column_config.NumberColumn(
         "Quantité",
-        help="Renseignez la quanitté",
+        help="Renseignez la quantité",
         format="%d",)
     show_dict = {"Nom":"Nom", "Prix":"Prix", "Catégorie":"Catégorie", "Quantité":xx, "Total":None}
     edited_order = st.data_editor(order_df, hide_index=True, disabled=[col for col in order_df if col != "Quantité"], column_config=show_dict)

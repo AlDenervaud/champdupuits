973aaf30b05acaf0a7ab221352767a8969cece31
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2025-02-06 11:40:33 +0100

    Update 2_Passer commande.py

--- pages/2_Passer commande.py
+++ pages/2_Passer commande.py
@@ -145,7 +145,8 @@ st.markdown("""Vous pouvez télécharger le bon et nous l'envoyer à lechampdupu
 try:
     order_df = st.session_state["order_df"]
     order_df["Quantité"] = order_df["Quantité"].apply(lambda x: str(x))
-    edited_order = st.data_editor(order_df, hide_index=True, disabled=[col for col in order_df if col != "Quantité"])
+    show_dict = {"Nom":"Nom", "Prix":"Prix", "Catégorie":"Catégorie", "Quantité":"Quantité", "Total":None}
+    edited_order = st.data_editor(order_df, hide_index=True, disabled=[col for col in order_df if col != "Quantité"], column_config=show_dict)
     
     # Reset order button
     if st.button("Réinitialiser la commande"):

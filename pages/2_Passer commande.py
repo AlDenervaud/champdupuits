c9377b4c2946decd288562a14ca6e7841ce339d1
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2025-02-06 11:32:44 +0100

    Update 2_Passer commande.py

--- pages/2_Passer commande.py
+++ pages/2_Passer commande.py
@@ -139,7 +139,7 @@ def GeneratePDF(df, client_name):
 st.title("Valider la commande et télécharger le bon")
 st.markdown("""Vous pouvez modifier les quantités dans le tableau ci-dessous (en kg ou en nombre d'unités).""")
 st.markdown("""Pour retirer un produit du panier, indiquez 0 dans la colonne \"Quantité\".
-La commande finale s'affiche dans le tableau en bas de la page.""")
+st.markdown("""La commande finale s'affiche dans le tableau en bas de la page.""")
 #st.markdown("""VoIndiquez les quantité en kg.""")
 st.markdown("""Vous pouvez télécharger le bon et nous l'envoyer à lechampdupuits@gmail.com.""")
 

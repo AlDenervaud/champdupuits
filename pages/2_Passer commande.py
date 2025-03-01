d959988fd29786f8200d78dfb2b09982df31e052
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2025-03-01 17:03:59 +0100

    finally getting somewhere

--- pages/2_Passer commande.py
+++ pages/2_Passer commande.py
@@ -7,32 +7,8 @@ from streamlit_js_eval import streamlit_js_eval
 from io import BytesIO
 from fpdf import FPDF
 # Custom
-from code.helper import test
+from pages.utils.helper import test
 
-
-def UpdateOrderFinal(order):
-    # Reset Total column
-    if "Total" in order.columns:
-        order.drop("Total", axis=1, inplace=True)
-    # Remove row with grand total
-    order = order[order["Nom"].str.strip() != ""]
-    
-    # Updates total price based on price and quantity
-    order["price_temp"] = order["Prix"].apply(lambda x: float(x.split(" ")[0].replace(",", ".")))
-    order["Quantité"] = pd.to_numeric(order["Quantité"], errors='coerce')
-    order["Total"] = order["price_temp"] * order["Quantité"]
-    
-    # Remove items with 0 quantity
-    order = order[order["Quantité"] != 0]
-    
-    # Add grand total
-    order = order._append({"Nom":"", "Prix":"", "Catégorie":"", "Quantité":"", "Total":order["Total"].sum()}, ignore_index=True)
-    order["Total"] = order["Total"].apply(lambda x: "{:.2f} €".format(x))
-
-    # Cleaning
-    final_order = order[['Nom', 'Prix', 'Catégorie', 'Quantité', 'Total']]
-
-    return final_order
 
 def ResetOrder():
     """Empties the order dataframe"""
--- pages/utils/helper.py
+++ pages/utils/helper.py
@@ -1,2 +1,25 @@
-def test():
-  print("Hello")
+import pandas as pd
+
+def UpdateOrderFinal(order):
+    # Reset Total column
+    if "Total" in order.columns:
+        order.drop("Total", axis=1, inplace=True)
+    # Remove row with grand total
+    order = order[order["Nom"].str.strip() != ""]
+    
+    # Updates total price based on price and quantity
+    order["price_temp"] = order["Prix"].apply(lambda x: float(x.split(" ")[0].replace(",", ".")))
+    order["Quantité"] = pd.to_numeric(order["Quantité"], errors='coerce')
+    order["Total"] = order["price_temp"] * order["Quantité"]
+    
+    # Remove items with 0 quantity
+    order = order[order["Quantité"] != 0]
+    
+    # Add grand total
+    order = order._append({"Nom":"", "Prix":"", "Catégorie":"", "Quantité":"", "Total":order["Total"].sum()}, ignore_index=True)
+    order["Total"] = order["Total"].apply(lambda x: "{:.2f} €".format(x))
+
+    # Cleaning
+    final_order = order[['Nom', 'Prix', 'Catégorie', 'Quantité', 'Total']]
+
+    return final_order
